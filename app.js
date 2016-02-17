2556e50d94033ecd92c58f4e5bd841c457bde578
pgangidi <pgangidi@kent.edu>
2016-02-17 16:47:06 +0000

    commit

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-var app = angular.module("sampleApp", ["firebase"]);
+var app=angular.module('myApp',['ngRoute','ngResource']);
+
+app.config(function($routeProvider){
+   
+    $routeProvider
+    
+    .when('/',{
+        templateUrl:'first.html',
+        controller:'homeController'
+    })
+    
+    .when('/forecast',{
+        templateUrl:'forecast.html',
+        controller:'forecastController'
+    })
+});
+
+app.controller('homeController',['$scope',function($scope){
+    
+}]);
+
+app.controller('forecastController',['$scope',function($scope){
+    
+}]);
+
